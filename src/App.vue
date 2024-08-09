57014e1f3f5e8f45639dc0e0ac8efbceceea9d8b
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-08-09 09:38:24 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <AppNavbar />
+    <AppJumbotron />
+    <AppCTA />
+    <AppCards />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppNavbar from './components/Navbar.vue'; // Updated import
+import AppJumbotron from './components/Jumbotron.vue';
+import AppCTA from './components/CTA.vue';
+import AppCards from './components/Cards.vue';
+import AppFooter from './components/Footer.vue';
+export default {
+  name: 'App',
+  components: {
+    AppNavbar, // Updated component name
+    AppJumbotron,
+    AppCTA,
+    AppCards,
+    AppFooter
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'; // Importer createApp depuis Vue 3
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+createApp(App).mount('#app');
